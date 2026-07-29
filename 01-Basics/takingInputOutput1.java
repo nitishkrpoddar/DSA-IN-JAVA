2dc1704a2f39c6ffd67547eb1b362d16877e66cf
Nitish Kumar Poddar <nitishkumarpoddar378@gmail.com>
2026-07-30 00:04:01 +0530

    Update and rename TakingInputOutput1.java to takingInputOutput1.java

--- 01-Basics/takingInputOutput1.java
+++ 01-Basics/takingInputOutput1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class takinginput{
+public class takinInputOutput1{
  public static void main(String[] args) {
  	Scanner sc=new Scanner(System.in);
  	int a=sc.nextInt();
