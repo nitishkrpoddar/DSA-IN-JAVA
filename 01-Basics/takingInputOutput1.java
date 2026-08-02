163de240321a9745afb96573cd40936026dd0e70
Nitish Kumar Poddar <nitishkumarpoddar378@gmail.com>
2026-08-02 15:30:48 +0530

    Add CheckNumberEvenOdd program

--- 01-Basics/takingInputOutput1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class takinInputOutput1{
- public static void main(String[] args) {
- 	Scanner sc=new Scanner(System.in);
- 	int a=sc.nextInt();
- 	System.out.println("number is="+a);
-	}
-} 
