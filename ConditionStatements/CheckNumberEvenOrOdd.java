3fc054fa7489ef5a549fb07537675711d2ce5a66
Nitish Kumar Poddar <nitishkumarpoddar378@gmail.com>
2026-08-02 16:07:10 +0530

    Add CheckNumberEvenOdd program

---
+++ ConditionStatements/CheckNumberEvenOrOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CheckNumberEvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        if(n%2==0){
+            System.out.print("Number is : Even");
+        }
+        else{
+            System.out.print("Number is : Odd");
+        }
+    }
+
+}
+
