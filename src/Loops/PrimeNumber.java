fa9481b3b6d395b322cc5b024ccf87f11f6075ce
Nitish Kumar Poddar <nitishkumarpoddar378@gmail.com>
2026-08-05 23:57:30 +0530

    Add print PrimeNumber program

---
+++ src/Loops/PrimeNumber.java
@@ -0,0 +1,20 @@
+//package Loops;
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number= ");
+        int n = sc.nextInt();
+        for (int i = 2; i <= n; i++) {
+            boolean isprime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isprime = false;
+                }
+            }
+            if (isprime) {
+                System.out.println("Number is prime =" + i);
+            }
+        }
+    }
+}
