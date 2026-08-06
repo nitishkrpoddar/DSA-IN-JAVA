fc8a8d7994b738c1a47ed2c63cde2f8e3ca26538
Nitish Kumar Poddar <nitishkumarpoddar378@gmail.com>
2026-08-06 08:42:44 +0530

    Add print CountDigit program

---
+++ Loops/CountDigit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CountDigit {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=n;i>0;i=i/10){
+            count++;
+        }
+        System.out.print(count);
+    }
+}
