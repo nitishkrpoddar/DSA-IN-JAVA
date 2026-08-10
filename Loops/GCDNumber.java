6177c35d4d390c6d67f3c9a8badf2c500322082c
Nitish Kumar Poddar <nitishkumarpoddar378@gmail.com>
2026-08-10 23:47:00 +0530

    Program of GCD Number useing while loop.

---
+++ Loops/GCDNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class GCDNumber {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        while(n2>0){
+            int temp=n2;
+            n2=n1%n2;
+            n1=temp;
+        }
+
+        System.out.print(n1);
+
+    }
+
+}
